76d33d1ccedbd1aed49074ac4d8c248f42012b64
CioSmart <vcioceanu@gmail.com>
2026-04-27 12:44:42 +0300

    fix supabase schema config

--- a.bat
+++ a.bat
@@ -1,9 +1,3 @@
-cd C:\Proiecte\aplicatie parcare\parkzen
-rm -rf .git
-git init
 git add .
-git commit -m "init: fork from parking-app"
-# creezi repo pe GitHub: parkzen
-git remote add origin https://github.com/CioSmart/parkzen.git
-git push -u origin main
-pause 0 
+git commit -m "fix supabase schema config"
+git push
